e9e6d75688e4640379ad3e937e97513a5e0505ac
it24104172 <it24104172@my.sliit.lk>
2025-09-16 21:05:06 +0530

    ps lab sheet 7

---
+++ Lab Sheet 07/it24104172.R
@@ -0,0 +1,12 @@
+punif(25,min=0,max=40,lower.tail=TRUE)-punif(10,min=0,max=40,lower.tail=TRUE)
+
+
+pexp(2,rate=1/3,lower.tail=TRUE)
+
+
+1 - pnorm(130,mean =100,sd=15,lower.tail=TRUE)
+
+pnorm(130,mean=100,sd=15,lower.tail=FALSE)
+
+
+qnorm(0.95,mean=100,sd=15,lower.tail=TRUE)
